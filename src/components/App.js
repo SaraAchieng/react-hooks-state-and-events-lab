9c9e23e8962310ea1000c43b849fe7e0f067dd8e
Sara Achieng <achieng997@gmail.com>
2024-08-03 11:05:17 +0300

    Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,56 @@
-import React from "react";
+// import React, {useState} from "react";
+// import ShoppingList from "./ShoppingList";
+// import itemData from "../data/items";
+
+// function App() {
+
+//   const [isDark, setIsDark] = useState(false)
+
+
+//   // replace 'false' with a state variable that can be toggled between true and false
+//   // this will be used for the Dark Mode Toggle feature
+//   const appClass = isDark ? "App dark" : "App light"
+//   function handleClick() {
+//     setIsDark((appClass)=> !appClass)
+//   }
+
+//   return (
+//     <div className={appClass}>
+//       <header>
+//         <h2>Shopster</h2>
+//         <button onClick={handleClick} >{isDark ? "Dark mode" : "Light mode"}</button>
+//       </header>
+//       <ShoppingList items={itemData} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+import React, {useState} from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
 
+  const [isDark, setIsDark] = useState(false)
+
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = isDark ? "App dark" : "App light"
+  function handleClick(){
+    setIsDark((appClass)=> !appClass)
+  }
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick ={handleClick} >{isDark ? "Dark Mode" : "Light Mode" }</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
@@ -20,3 +58,9 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+  const inCart = isInCart ? "in-cart" : "";
+
+  function handleCartClick() {
+    setIsInCart((inCart) => !inCart)
+
+  }
+
+
+
   return (
-    <li className="">
+    <li className={inCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCartClick}>{!isInCart ? "Add to Cart" : "Remove From Cart" }</button>
     </li>
   );
 }
 
 export default Item;
+
+
